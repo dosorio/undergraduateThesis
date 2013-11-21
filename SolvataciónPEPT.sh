aec464f0f6ecdd8977e5c1862c48ce4202e8e566
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2013-11-21 06:44:41 -0500

    Modificación Script Solvatación

--- SolvataciónPEPT.sh
+++ SolvataciónPEPT.sh
@@ -1,377 +1,169 @@
 #!/bin/sh
 
-#  InterPEPT.sh
-#  SCRIPT PARA EVALUAR LA INTERACCIÃ“N DE PÃ‰PTIDOS CON BICAPAS LIPÃDICAS
+#  SOLV.sh
+#  SCRIPT PARA EVALUAR LA ESTABILIDAD DE LOS PÃ‰PTIDOS CATIÃ“NICOS ANTIMICROBIANOS EN AGUA
 #  DANIEL CAMILO OSORIO
 #  UNIVERSIDAD INDUSTRIAL DE SANTANDER
 #  d.osorio@me.com
-
-
 
 # PATH
 export PATH=$PATH:/usr/bin:/usr/local/cuda/bin:/usr/local/gromacs/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/gromacs/lib
 
-# CREANDO CARPETA DE RESULTADOS
-mkdir PEP-MEM ; cd PEP-MEM
-
 # DEFINIENDO PÃ‰PTIDOS A SIMULAR
-for pept in 1T51 1KUW  1WO0  2AP7  2K6O  2LNF  2LQA  2RLH  4B2U  1OT0  1X22  2B68  2KAM  2LO7  2M0D  2RSH  4BMF 1S6W  1Z64  2JQ0  2KHF  2LQ0  2M9I  3Q8J  2AMN  2K10  2LL1  2LQ1  2MAG  4B19
+for pept in 1KUW  1WO0  2AP7  2K6O  2LNF  2LQA  2RLH  4B2U  1OT0  1X22  2B68  2KAM  2LO7  2M0D  2RSH  4BMF 1S6W  1Z64  2JQ0  2KHF  2LQ0  2M9I  3Q8J 1T51  2AMN  2K10  2LL1  2LQ1  2MAG  4B19
 do
 
-# CREANDO LA CARPETA DEL PÃ‰PTIDO
-mkdir $pept ; cd $pept
+# CREANDO CARPETAS DE SIMULACIÃ“N
+mkdir ~/SOLV
+mkdir ~/SOLV/$pept
+cp ~/PDB/$pept.pdb ~/SOLV/$pept/
+cd ~/SOLV/$pept/
 
-for memb in POPC #POPE POPG
-do
-
-# CREANDO CARPETA EN RESULTADOS
-mkdir $memb ; cd $memb
-
-# INCLUYE LAS CONFIGURACIONES Y ESTRUCTURAS
-cp ~/PDB/$pept.pdb ./
-cp ~/MEM/$memb* ./
-cp -r ~/FF/* ./
-
-if [ $memb = POPG ];
-then
-cp ~/MEM/dPOPG.itp ./
-fi
-
-#CREAR LA TOPOLOGIA
-pdb2gmx_mpi -f $pept.pdb -o $pept.gro -ignh -water spc<<EOF
-14
+# GENERA EL ARCHIVO DE ADICIÃ“N DE IONES
+cat > ions.mdp << EOF
+integrator      = steep
+emtol		= 1000.0
+emstep          = 0.01
+nsteps          = 50000
+nstlist         = 1
+ns_type         = grid
+rlist		= 1.0
+coulombtype     = PME
+rcoulomb        = 1.0
+rvdw		= 1.0
+pbc             = xyz
 EOF
 
-#ELIMINAR LA PERIODICIDAD DE LA MEMBRANA
-#editconf -f $membrana.pdb -o $membrana.gro -pbc
-
-# CREANDO EL ARCHIVO MINIM.MDP
+# GENERA EL ARCHIVO DE MINIMIZACIÃ“N DE ENERGÃA
 cat > minim.mdp << EOF
 integrator	= steep
 emtol		= 1000.0
-emstep      = 0.01
+emstep      	= 0.01
 nsteps		= 50000
 nstlist		= 1
 ns_type		= grid
-rlist		= 1.2
+rlist		= 1.0
 coulombtype	= PME
-rcoulomb	= 1.2
-rvdw		= 1.2
-pbc         = xyz
+rcoulomb	= 1.0
+rvdw		= 1.0
+pbc         	= xyz
 EOF
 
-# CONFIGURAR EL T=0 DE LA MEMBRANA
-grompp_mpi -f minim.mdp -c $memb.gro -p $memb.top -o em.tpr
-
-# CREAR LA CAJA DE SIMULACIÃƒâ€œN DE LA MEMBRANA
-trjconv_mpi -s em.tpr -f $memb.gro -o n-$memb.gro -pbc mol -ur compact <<EOF
-2
+# CONVERSIÃ“N DEL PDB AL GRO
+pdb2gmx_mpi -f $pept.pdb -o $pept.gro -ignh <<EOF
+13	# Utiliza el Campo de Fuerza Gromos53a6
+1	# SPC Modelo de Agua
 EOF
 
-# AUMENTANDO TAMAÃ‘O EN EL EJE Z
-if [ $memb = POPC ];
+
+# DEFINIR EL TAMAÃ‘O DE LA CAJA DE SIMULACIÃ“N
+editconf_mpi -f $pept.gro -o $pept-NB.gro -c -d 1.0 -bt cubic
+
+# AÃ‘ADIR MOLECULAS DE AGUA
+genbox_mpi -cp $pept-NB.gro -cs spc216.gro -o $pept-SOLV.gro -p topol.top
+
+# CONFIGURAR LA ADICIÃ“N DE IONES
+grompp_mpi -f ions.mdp -c $pept-SOLV.gro -p topol.top -o ions.tpr
+
+# AÃ‘ADIR IONES
+genion_mpi -s ions.tpr -o $pept-ION.gro -p topol.top -pname NA -nname CL -neutral <<EOF
+13 # AÃ±ade los iones necesarios al solvente para estabilizar el sistema
+EOF
+
+# MINIMIZACIÃ“N DE ENERGÃA
+if [ $pept = 1KUW ] || [ $pept = 2M0D ] || [ $pept = 4B2U ] || [ $pept = 4BMF ];
 then
-editconf_mpi -f n-$memb.gro -o w-$memb.gro -c -box 6.48650   6.48650    7.50
+grompp_mpi -f minim.mdp -c $pept-SOLV.gro -p topol.top -o em-$pept.tpr
+mpirun -np 8 mdrun_mpi -v -deffnm em-$pept
 else
-if [ $memb = POPE ];
-then
-editconf_mpi -f n-$memb.gro -o w-$memb.gro -c -box 9.57070   9.48610    7.50
-else
-if [ $memb = POPG ];
-then
-editconf_mpi -f n-$memb.gro -o w-$memb.gro -c -box 6.63780   6.62730    7.50
-fi
-fi
+grompp_mpi -f minim.mdp -c $pept-ION.gro -p topol.top -o em-$pept.tpr
+mpirun -np 8 mdrun_mpi -v -deffnm em-$pept
 fi
 
-# POSICIONANDO EL PEPTIDO
-if [ $memb = POPC ];
-then
-editconf_mpi -f $pept.gro -o nb-$pept.gro -box 6.48650   6.48650   7.50000 -center 3.24325  3.24325  0.5
-else
-if [ $memb = POPE ];
-then
-editconf_mpi -f $pept.gro -o nb-$pept.gro -box 9.57070   9.48610   7.50000 -center 4.78535  4.74305  0.5
-else
-if [ $memb = POPG ];
-then
-editconf_mpi -f $pept.gro -o nb-$pept.gro -box 6.63780   6.62730   7.50000 -center 3.31800  3.31300  0.5
-fi
-fi
-fi
-
-# CREANDO EL ARCHIVO DE UNIÃ“N DE TOPOLOGÃAS
-cp ~/CONFIG/UnirGRO.py ./
-
-#CREAR LA CAJA CONJUNTA DE SIMULACIÃƒâ€œN
-cat nb-$pept.gro w-$memb.gro > system.gro
-python UnirGRO.py
-rm system.gro
-mv system2.gro $pept-$memb.gro
-
-# UNIENDO TOPOLOGÃAS
-cp ~/CONFIG/$memb.py ./
-python $memb.py
-
-# RENOMBRAMIENTO DE ARCHIVOS
-rm topol.top
-mv topol2.top topol.top
-
-# CREACIÃ“N DEL ARCHIVO VDWRADII PARA SOLVATACIÃ“N
-cat > vdwradii.dat <<EOF
-; Very approximate VanderWaals radii
-; only used for drawing atoms as balls or for calculating atomic overlap.
-; longest matches are used
-; '???' or '*' matches any residue name
-; 'AAA' matches any protein residue name
-???  C     0.375
-???  F     0.12
-???  H     0.04
-???  N     0.110
-???  O     0.105
-???  S     0.16
-; Water charge sites
-SOL  MW    0
-SOL  LP    0
-; Masses for vsite construction
-GLY  MN1   0
-GLY  MN2   0
-ALA  MCB1  0
-ALA  MCB2  0
-VAL  MCG1  0
-VAL  MCG2  0
-ILE  MCG1  0
-ILE  MCG2  0
-ILE  MCD1  0
-ILE  MCD2  0
-LEU  MCD1  0
-LEU  MCD2  0
-MET  MCE1  0
-MET  MCE2  0
-TRP  MTRP1 0
-TRP  MTRP2 0
-THR  MCG1  0
-THR  MCG2  0
-LYSH MNZ1  0
-LYSH MNZ2  0
+# GRAFICO DE MINIMIZACIÃ“N DE ENERGÃA
+g_energy_mpi -f em-$pept.edr -o epot-$pept.xvg<<EOF
+10
+0
 EOF
 
-#AÃƒâ€˜ADIR MOLECULAS DE AGUA
-genbox_mpi -cp $pept-$memb.gro -cs spc216.gro -o $pept-$memb-solv.gro -p topol.top
-
-#ELIMINAR EL ;VDW
-rm vdwradii.dat
-
-# CREANDO EL ARCHIVO ions.mdp
-cat > ions.mdp <<EOF
-integrator	= steep
-emtol		= 1000.0
-emstep 	        = 0.01
-nsteps		= 50000
-nstlist		= 1
-ns_type		= grid
-rlist		= 1.2
-coulombtype	= PME
-rcoulomb	= 1.2
-rvdw		= 1.2
-pbc		= xyz
-EOF
-
-#CONFIGURAR LA ADICIÃƒâ€œN DE IONES
-grompp_mpi -f ions.mdp -c $pept-$memb-solv.gro -p topol.top -o ions.tpr
-
-
-#AÃƒâ€˜ADIR IONES HASTA NEUTRALIZAR LA CARGA
-genion_mpi -s ions.tpr -o i-$pept-$memb.gro -p topol.top -pname NA -nname CL -neutral<<EOF
-15
-EOF
-
-#CONFIGURACIÃƒâ€œN DE LA MINIMIZACIÃƒâ€œN DE ENERGÃƒÂA
-grompp_mpi -f minim.mdp -c i-$pept-$memb.gro -p topol.top -o $pept-$memb.tpr
-
-#MINIMIZACIÃƒâ€œN HASTA 1000KJ/MOL
-mdrun_mpi -v -deffnm $pept-$memb
-
-#HACER GRUPOS DE SIMULACIÃƒâ€œN
-make_ndx_mpi -f $pept-$memb.gro -o index-$pept-$memb.ndx <<EOF
-16 | 14
-1 | 13
-q
-EOF
-
-# SIMULACIONES A DIFERENTES TEMPERATURAS
 for temp in 293 310 323
 do
-if [ $memb = POPG ];
-then
-cat > nvt.mdp <<EOF
-title					= NVT
-integrator				= md
-nsteps					= 500000
+
+cat > nvt.mdp << EOF
+integrator              = md
+nsteps                  = 50000
 dt                      = 0.002
-nstxout					= 100
-nstvout					= 100
-nstenergy				= 100
-nstlog					= 100
-continuation				= no
-constraint_algorithm 	= lincs
-constraints				= all-bonds
-lincs_iter				= 1
-lincs_order				= 4
-ns_type					= grid
-nstlist					= 5
-rlist					= 1.2
-rcoulomb				= 1.2
-rvdw					= 1.2
-coulombtype				= PME
-pme_order				= 4
-fourierspacing			= 0.16
-tcoupl					= V-rescale
-tc-grps					= Protein DPOPG SOL_NA
-tau_t					= 0.1	0.1	0.1
-ref_t					= $temp $temp $temp
-pcoupl					= no
-pbc		    			= xyz
-DispCorr				= EnerPres
-gen_vel					= yes
-gen_temp				= $temp
-gen_seed				= -1
-nstcomm					= 1
-comm-mode				= Linear
-comm-grps				= Protein_DPOPG SOL_NA
+nstxout                 = 100
+nstvout                 = 100
+nstenergy               = 100
+nstlog                  = 100
+continuation            = no
+constraint_algorithm    = lincs
+constraints             = all-bonds
+lincs_iter              = 1
+lincs_order             = 4
+ns_type                 = grid
+nstlist                 = 5
+rlist                   = 1.0
+rcoulomb                = 1.0
+rvdw                    = 1.0
+coulombtype             = PME
+pme_order               = 4
+fourierspacing          = 0.16
+tcoupl                  = V-rescale
+tc-grps                 = Protein Non-Protein
+tau_t                   = 0.1	0.1
+ref_t                   = $temp $temp
+pcoupl                  = no
+pbc                     = xyz
+DispCorr                = EnerPres
+gen_vel                 = yes
+gen_temp                = 293
+gen_seed                = -1
 cutoff-scheme           = Verlet
 EOF
-else
-cat > nvt.mdp <<EOF
-title					= NVT
-integrator				= md
-nsteps					= 500000
-dt		   			 	= 0.002
-nstxout					= 100
-nstvout					= 100
-nstenergy				= 100
-nstlog					= 100
-continuation			= no
-constraint_algorithm 	= lincs
-constraints				= all-bonds
-lincs_iter				= 1
-lincs_order				= 4
-ns_type					= grid
-nstlist					= 5
-rlist					= 1.2
-rcoulomb				= 1.2
-rvdw					= 1.2
-coulombtype				= PME
-pme_order				= 4
-fourierspacing			= 0.16
-tcoupl					= V-rescale
-tc-grps					= Protein $memb SOL_CL
-tau_t					= 0.1	0.1	0.1
-ref_t					= $temp $temp $temp
-pcoupl					= no
-pbc		    			= xyz
-DispCorr				= EnerPres
-gen_vel					= yes
-gen_temp				= $temp
-gen_seed				= -1
-nstcomm					= 1
-comm-mode				= Linear
-comm-grps				= Protein_$memb SOL_CL
+
+cat > npt.mdp << EOF
+integrator              = md
+nsteps                  = 50000
+dt                      = 0.002
+nstxout                 = 100
+nstvout                 = 100
+nstenergy               = 100
+nstlog                  = 100
+continuation            = yes
+constraint_algorithm    = lincs
+constraints             = all-bonds
+lincs_iter              = 1
+lincs_order             = 4
+ns_type                 = grid
+nstlist                 = 5
+rlist                   = 1.0
+rcoulomb                = 1.0
+rvdw                    = 1.0
+coulombtype             = PME
+pme_order               = 4
+fourierspacing          = 0.16
+tcoupl                  = V-rescale
+tc-grps                 = Protein Non-Protein
+tau_t                   = 0.1	0.1
+ref_t                   = $temp $temp
+pcoupl                  = Parrinello-Rahman
+pcoupltype              = isotropic
+tau_p                   = 2.0
+ref_p                   = 1.0
+compressibility         = 4.5e-5
+refcoord_scaling        = com
+pbc                     = xyz
+DispCorr                = EnerPres
+gen_vel                 = no
 cutoff-scheme           = Verlet
 EOF
-fi
 
-#ACOPLAMIENTO NVT EN X TEMPERATURA
-grompp_mpi -f nvt.mdp -c $pept-$memb.gro -p topol.top -n index-$pept-$memb.ndx -o nvt-$pept-$memb-$temp.tpr
-mpirun -np 8 mdrun_mpi -v -deffnm nvt-$pept-$memb-$temp
-
-# CREANDO EL ARCHIVO npt.mdp
-if [ $memb = POPG ]
-then
-cat > npt.mdp << EOF
-integrator          = md
-nsteps              = 500000
-dt                  = 0.002
-nstxout             = 100
-nstvout             = 100
-nstenergy           = 100
-nstlog              = 100
-continuation        = yes
-constraint_algorithm = lincs
-constraints         = all-bonds
-lincs_iter          = 1
-lincs_order         = 4
-ns_type             = grid
-nstlist             = 5
-rlist               = 1.2
-rcoulomb            = 1.2
-rvdw                = 1.2
-coulombtype         = PME
-pme_order           = 4
-fourierspacing      = 0.16
-tcoupl              = Nose-Hoover
-tc-grps             = Protein DPOPG	SOL_NA
-tau_t               = 0.5	0.5	0.5
-ref_t               = $temp $temp $temp
-pcoupl              = Parrinello-Rahman
-pcoupltype          = semiisotropic
-tau_p               = 5.0
-ref_p               = 1.0	1.0
-compressibility     = 4.5e-5	4.5e-5
-pbc                 = xyz
-DispCorr            = EnerPres
-gen_vel             = no
-cutoff-scheme       = Verlet
-EOF
-else
-cat > npt.mdp << EOF
-integrator          = md
-nsteps              = 500000
-dt                  = 0.002
-nstxout             = 100
-nstvout             = 100
-nstenergy           = 100
-nstlog              = 100
-continuation        = yes
-constraint_algorithm = lincs
-constraints         = all-bonds
-lincs_iter          = 1
-lincs_order         = 4
-ns_type             = grid
-nstlist             = 5
-rlist               = 1.2
-rcoulomb            = 1.2
-rvdw                = 1.2
-coulombtype         = PME
-pme_order           = 4
-fourierspacing      = 0.16
-tcoupl              = Nose-Hoover
-tc-grps             = Protein $memb	SOL_CL
-tau_t               = 0.5	0.5	0.5
-ref_t               = $temp $temp $temp
-pcoupl              = Parrinello-Rahman
-pcoupltype          = semiisotropic
-tau_p               = 5.0
-ref_p               = 1.0	1.0
-compressibility     = 4.5e-5	4.5e-5
-pbc                 = xyz
-DispCorr            = EnerPres
-gen_vel             = no
-cutoff-scheme       = Verlet
-EOF
-fi
-
-#ACOPLAMIENTO NPT EN X TEMPERATURA
-grompp_mpi -f npt.mdp -c nvt-$pept-$memb-$temp.gro -t nvt-$pept-$memb-$temp.cpt -p topol.top -n index-$pept-$memb.ndx -o npt-$pept-$memb-$temp.tpr
-mpirun -np 8 mdrun_mpi -deffnm npt-$pept-$memb-$temp -v
-
-if [ $memb = POPG ]
-then
-# CREANDO EL ARCHIVO md.mdp
 cat > md.mdp << EOF
 integrator              = md
-nsteps                  = 25000000
+nsteps                  = 1000000
 dt                      = 0.002
 nstxout                 = 1000
 nstvout                 = 1000
@@ -385,72 +177,72 @@ lincs_iter              = 1
 lincs_order             = 4
 ns_type                 = grid
 nstlist                 = 5
-rlist                   = 1.2
-rcoulomb                = 1.2
-rvdw                    = 1.2
+rlist                   = 1.0
+rcoulomb                = 1.0
+rvdw                    = 1.0
 coulombtype             = PME
 pme_order               = 4
 fourierspacing          = 0.16
-tcoupl                  = Nose-Hoover
-tc-grps                 = Protein DPOPG	SOL_NA
-tau_t                   = 0.5	0.5	0.5
-ref_t                   = $temp $temp $temp
+tcoupl                  = V-rescale
+tc-grps                 = Protein Non-Protein
+tau_t                   = 0.1	0.1
+ref_t                   = $temp $temp
 pcoupl                  = Parrinello-Rahman
-pcoupltype              = semiisotropic
+pcoupltype              = isotropic
 tau_p                   = 2.0
-ref_p                   = 1.0	1.0
-compressibility         = 4.5e-5	4.5e-5
+ref_p                   = 1.0
+compressibility         = 4.5e-5
 pbc                     = xyz
 DispCorr                = EnerPres
 gen_vel                 = no
 cutoff-scheme           = Verlet
 EOF
-else
-cat > md.mdp << EOF
-integrator              = md
-nsteps                  = 25000000
-dt                      = 0.002
-nstxout                 = 1000
-nstvout                 = 1000
-nstxtcout               = 1000
-nstenergy               = 1000
-nstlog                  = 1000
-continuation            = yes
-constraint_algorithm    = lincs
-constraints             = all-bonds
-lincs_iter              = 1
-lincs_order             = 4
-ns_type                 = grid
-nstlist                 = 5
-rlist                   = 1.2
-rcoulomb                = 1.2
-rvdw                    = 1.2
-coulombtype             = PME
-pme_order               = 4
-fourierspacing          = 0.16
-tcoupl                  = Nose-Hoover
-tc-grps                 = Protein $memb	SOL_CL
-tau_t                   = 0.5	0.5	0.5
-ref_t                   = $temp $temp $temp
-pcoupl                  = Parrinello-Rahman
-pcoupltype              = semiisotropic
-tau_p                   = 2.0
-ref_p                   = 1.0	1.0
-compressibility         = 4.5e-5	4.5e-5
-pbc                     = xyz
-DispCorr                = EnerPres
-gen_vel                 = no
-cutoff-scheme           = Verlet
+
+# ACOPLAMIENTO NVT
+grompp_mpi -f nvt.mdp -c em-$pept.gro -p topol.top -o nvt-$pept-$temp.tpr
+mpirun -np 8 mdrun_mpi -v -deffnm nvt-$pept-$temp
+
+# GRÃFICO DE TEMPERATURA
+g_energy_mpi -f nvt-$pept-$temp.edr -o $temp-$pept-temp.xvg<<EOF
+14
+0
 EOF
-fi
 
+# ACOPLAMIENTO NPT
+grompp_mpi -f npt.mdp -c nvt-$pept-$temp.gro -t nvt-$pept-$temp.cpt -p topol.top -o npt-$pept-$temp.tpr
+mpirun -np 8 mdrun_mpi -v -deffnm npt-$pept-$temp
 
-#DINÃƒÂMICA MOLECULAR
-grompp_mpi -f md.mdp -c npt-$pept-$memb-$temp.gro -t npt-$pept-$memb-$temp.cpt -p topol.top -n index-$pept-$memb.ndx -o md-$pept-$memb-$temp.tpr
-mpirun -np 8 mdrun_mpi -deffnm md-$pept-$memb-$temp -v
+# GRÃFICO DE PRESIÃ“N
+g_energy_mpi -f npt-$pept-$temp.edr -o pres-$pept-$temp.xvg<<EOF
+15
+0
+EOF
+
+# GRÃFICO DE DENSIDAD
+g_energy_mpi -f npt-$pept-$temp.edr -o den-$pept-$temp.xvg<<EOF
+21
+0
+EOF
+
+# DINÃMICA MOLECULAR
+grompp_mpi -f md.mdp -c npt-$pept-$temp.gro -t npt-$pept-$temp.cpt -p topol.top -o md-$pept-$temp.tpr
+mpirun -np 8 mdrun_mpi -v -deffnm md-$pept-$temp
+
+# CONVIRTIENDO LAS TRAYECTORIAS
+trjconv_mpi -s md-$pept-$temp.tpr -f md-$pept-$temp.xtc -o nopbc-$pept-$temp.xtc -pbc mol -ur compact <<EOF
+0
+EOF
+
+# RMSD
+g_rms_mpi -s em-$pept.tpr -f nopbc-$pept-$temp.xtc -o rmsd-$pept-$temp.xvg -tu ns <<EOF
+4
+4
+EOF
+
+# GIRO
+g_girate_mpi -s md-$pept-$temp.tpr -f nopbc-$pept-$temp.xtc -o giro-$pept-$temp.xvg<<EOF
+1
+EOF
 done
-cd ..
+cd $SOLV
 done
-cd ..
-done
-exit
