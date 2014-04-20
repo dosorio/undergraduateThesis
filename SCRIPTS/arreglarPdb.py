522f55e423e570acca968d6c57cef1498086a3c9
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-04-20 16:50:15 -0500

    UPDATE Results

---
+++ SCRIPTS/arreglarPdb.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  4 10:45:46 2014
+
+"""
+
+
+import glob
+import os
+os.chdir("/Users/Daniel/Desktop/POPG")
+for file in glob.glob("*.pdb"):
+    inFile=open(file, 'r')
+    outFile=open(file+'-A','w')
+    
+    for line in iter(inFile):
+        
+        if line.find('ATOM')>=0:
+            name=line[12:16]
+            temp=name[0]
+            
+            if temp.isdigit():
+                newLine=line[0:12]+name[1:len(name)]+'\t' +' ' +line[17:len(line)]
+                
+                outFile.write(newLine)
+                
+            else:
+                outFile.write(line)
+        else:
+            outFile.write(line)           
+        
+    inFile.close()
+    outFile.close()
+            
+        
+
